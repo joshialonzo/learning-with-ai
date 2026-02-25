8ebdd2aaefe4fe41378b9681f296f676f55b2066
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-02-24 18:13:59 -0600

    Tutorial 2: FastAPI application

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello World!")
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
