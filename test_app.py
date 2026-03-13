68533dd9672340e6d069639526e59c1fab3ac830
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-03-13 17:56:55 -0600

    First commit

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import httpx
-
-
-def test_root():
-    response = httpx.get("http://127.0.0.1:8000/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
--- test_db.py
+++
@@ -1,7 +0,0 @@
-import httpx
-
-
-def test_db_status():
-    response = httpx.get("http://localhost:8000/db-status")
-    assert response.status_code == 200
-    assert response.json()["db"] == "connected"
